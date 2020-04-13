66a33653ca9f4f7b1e48387a3372143bb9ef6c84
wangjianpeng <707804052@qq.com>
2020-04-13 20:45:48 +0800

    nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,14 @@
 <template>
   <div id="app">
     <RouterView />
+    <BottomNavBar />
   </div>
 </template>
+<script>
+import BottomNavBar from '@/components/BottomNavBar'
+export default {
+  components: {
+    BottomNavBar
+  }
+}
+</script>
---
+++ src/components/BottomNavBar/index.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="bottom-nav-bar">
+    <div class="nav-item active">
+      <figure class="iconfont iconhome1"></figure>
+      <span>首页</span>
+    </div>
+    <div class="nav-item">
+      <figure class="iconfont iconfenlei"></figure>
+      <span>分类</span>
+    </div>
+    <div class="nav-item">
+      <figure class="iconfont icongouwuche"></figure>
+      <span>购物车</span>
+    </div>
+    <div class="nav-item">
+      <figure class="iconfont icongerenzhongxin"></figure>
+      <span>个人中心</span>
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.bottom-nav-bar {
+  position: fixed;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  z-index: 99;
+  display: flex;
+  width: 100%;
+  height: 113px;
+  padding: 0 70px;
+  box-shadow: 0 -11px 32px rgba(0, 0, 0, 0.08);
+}
+
+.nav-item {
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  color: #9a9a9a;
+
+  &.active {
+    color: #d44d44;
+  }
+
+  .iconfont {
+    font-size: 40px;
+  }
+
+  span {
+    padding-top: 10px;
+    font-size: 20px;
+  }
+}
+</style>
