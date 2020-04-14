3c0f7b474df53b924812d64d8c0d9439f06ee885
wangjianpeng <707804052@qq.com>
2020-04-14 20:08:38 +0800

    底部tabbar

--- src/components/BottomNavBar/index.vue
+++ src/components/BottomNavBar/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="bottom-nav-bar">
-    <VanTabbar>
-      <VanTabbarItem icon="home-o">首页</VanTabbarItem>
-      <VanTabbarItem icon="wap-nav">分类</VanTabbarItem>
-      <VanTabbarItem icon="shopping-cart">购物车</VanTabbarItem>
-      <VanTabbarItem icon="manager">我的</VanTabbarItem>
+    <VanTabbar route>
+      <VanTabbarItem icon="home-o" to="/">首页</VanTabbarItem>
+      <VanTabbarItem icon="wap-nav" to="/category">分类</VanTabbarItem>
+      <VanTabbarItem icon="shopping-cart" to="/cart">购物车</VanTabbarItem>
+      <VanTabbarItem icon="manager" to="/user">我的</VanTabbarItem>
     </VanTabbar>
   </div>
 </template>
