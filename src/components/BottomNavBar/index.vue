b9ede3f2b8eda4832d6eb45cdca027683b43a6de
wangjianpeng <707804052@qq.com>
2020-04-15 19:54:33 +0800

    删除底部bar样式

--- src/components/BottomNavBar/index.vue
+++ src/components/BottomNavBar/index.vue
@@ -8,37 +8,3 @@
     </VanTabbar>
   </div>
 </template>
-<style lang="scss" scoped>
-.bottom-nav-bar {
-  position: fixed;
-  left: 0;
-  right: 0;
-  bottom: 0;
-  z-index: 99;
-  display: flex;
-  width: 100%;
-  background-color: #fff;
-}
-
-.nav-item {
-  @include horVerCenter;
-
-  flex-direction: column;
-  flex: 1;
-  justify-content: center;
-  color: #9a9a9a;
-
-  &.active {
-    color: #d44d44;
-  }
-
-  .iconfont {
-    font-size: 40px;
-  }
-
-  span {
-    padding-top: 10px;
-    font-size: 20px;
-  }
-}
-</style>
